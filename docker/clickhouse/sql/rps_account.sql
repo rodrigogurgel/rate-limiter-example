149561f024c707418524f9747796eaa71baf6a99
Rodrio Gurgel <rodrigohenrique.gurgel@outlook.com>
2025-09-20 20:46:59 -0300

    fix: SQL query

--- docker/clickhouse/sql/rps_account.sql
+++ docker/clickhouse/sql/rps_account.sql
@@ -1,25 +1,28 @@
-WITH now() - INTERVAL 1 MINUTE AS t_from, now() AS t_to
+-- CASO A QUERY RETORNE VALORES ELEVADOS EM DETERMINADO INTERVALO, VERIFIQUE SE A VARIAÇÃO NO CAMPO "DATETIME" FOI MAIOR OU MENOR DO QUE AS VARIAÇÕES ANTERIORES
+-- VARIAÇÕES PARA MAIS  = METRICAS TRIPLICADAS
+-- VARIAÇÕES PARA MENOS = METRICAS DUPLICADAS
+WITH now() - INTERVAL 10 MINUTE AS t_from, now() AS t_to
 SELECT
-    second, account, allowed, sum (delta) AS rps
+    datetime, account, allowed, sum (delta) AS rps
 FROM
     (
-    SELECT
-    toStartOfSecond(ts) AS second, -- remove ms
-    account, allowed, delta
-    FROM
-    (
-    SELECT
-    d.timestamp AS ts, lagInFrame(d.timestamp) OVER (PARTITION BY d.id ORDER BY d.timestamp) AS prev_ts, t.tags['account'] AS account, t.tags['allowed'] AS allowed, greatest(
-    d.value - lagInFrame(d.value) OVER (PARTITION BY d.id ORDER BY d.timestamp), 0
-    ) AS delta
-    FROM timeSeriesData('metrics', 'prom_rw') AS d
-    JOIN timeSeriesTags('metrics', 'prom_rw') AS t USING id
-    WHERE t.metric_name = 'app_rate_limit_account_requests_total'
-    AND d.timestamp <= t_to
-    AND t.tags['allowed'] = 'true'
+        SELECT
+            ts AS datetime,
+            account, allowed, delta
+        FROM
+            (
+                SELECT
+                    d.timestamp AS ts, lagInFrame(d.timestamp) OVER (PARTITION BY d.id ORDER BY d.timestamp) AS prev_ts, t.tags['account'] AS account, t.tags['allowed'] AS allowed, greatest(
+                        d.value - lagInFrame(d.value) OVER (PARTITION BY d.id ORDER BY d.timestamp), 0
+                                                                                                                                                                                     ) AS delta
+                FROM timeSeriesData('metrics', 'prom_rw') AS d
+                         JOIN timeSeriesTags('metrics', 'prom_rw') AS t USING id
+                WHERE t.metric_name = 'app_rate_limit_account_requests_total'
+                  AND d.timestamp <= t_to
+                  AND t.tags['allowed'] = 'true'
+            )
+        WHERE ts BETWEEN t_from AND t_to
+          AND prev_ts >= t_from          -- delta totalmente dentro da janela
     )
-    WHERE ts BETWEEN t_from AND t_to
-    AND prev_ts >= t_from          -- delta totalmente dentro da janela
-    )
-GROUP BY second, account, allowed
-ORDER BY second, account, allowed;
+GROUP BY datetime, account, allowed
+ORDER BY datetime, account, allowed;
